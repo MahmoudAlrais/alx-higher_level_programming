4fc8d852681d8b61d76d5dd81ed6da7da8872691
MahmoudAlrais <mahmoudalraismerch@gmail.com>
2023-09-11 02:18:31 -0700

    0x0A-python-inheritance 4

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-"""Defines Rectangle module."""
 Rectangle = __import__('9-rectangle').Rectangle
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
 
 
 class Square(Rectangle):
-    """class body."""
+    """Square class that inherits from Rectangle that inherits BaseGeometry"""
 
     def __init__(self, size):
+        """Method for initialized attrubutes"""
 
-        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.integer_validator("size", size)
         self.__size = size
+
+    def area(self):
+        """rectangle area"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
